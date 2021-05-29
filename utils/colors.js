b45e74c068b4031d1a7659fc61b826b48bb05c79
Lucas Zanganelli <lucasfzanganelli@gmail.com>
2021-05-28 23:30:47 -0300

    colors.js
    Criei a pasta utils para adicionar o arquivo colors.js
    
    No arquivo colors.jss adicionei as cores do card txt.

---
+++ utils/colors.js
@@ -0,0 +1,8 @@
+export const colors = Object.freeze({
+    light: '#EBF5F7',
+    dark: '#444444',
+    cardLight: '#FFFFFF',
+    cardDark: '#555555',
+    txtLight: '#000000',
+    txtDark: '#FFFFFF',
+})
