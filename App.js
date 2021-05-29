0b3a5b4315169eb802117f7f4b81a5600c7b73ab
Lucas Zanganelli <lucasfzanganelli@gmail.com>
2021-05-28 23:29:00 -0300

    App.js
    adicionei importação arquivo utils,
    adicionei importacao do react native vector
    Adicionei style na view
    adicionei switch e nome
    Alterei o SafeAreaView
    adicionei bgColor e txtcolor

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, SafeAreaView, FlatList, StatusBar, TouchableOpacity } from 'react-native';
 import CardQuote from './components/card/card.component';
 import {Switch} from 'react-native-paper'
+import {colors} from './utils/colors';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 
 const notList = [
   { id: 1, task: 'Suco de gratidão + clorofila', background: 'https://image.freepik.com/free-vector/flat-night-sky-background_23-2148032671.jpg'},
@@ -14,21 +18,28 @@ export default function App() {
 
 	const _handleTheme = () => setTheme(!theme)
 
+  const icone = theme ? 'moon-o' : 'sun-o'
+
 
   return (
 
-    <SafeAreaView style={[styles.container, {backgroundColor: theme ? '#444444' : '#EBF5F7'}]}>
+    <SafeAreaView style={[styles.container, {backgroundColor: theme ? colors.dark : colors.light}]}>
       <StatusBar
         animated={true}
         backgroundColor={"#c64242"}
       />
-		<Switch value={theme} onValueChange={_handleTheme} />
+    <View style={{width: '100%', paddingHorizontal: 30, flexDirection: 'row', paddingTop: 10, alignItems: 'center', justifyContent: 'flex-end'}}>
+      <Switch style={{marginRight: 5}} value={theme} onValueChange={_handleTheme} />
+      <Icon name={icone} size={18} color="#999" />
+    </View>
 
         <FlatList data ={notList}
           CardExtractor={item => item.id}
           renderItem={({ item }) => 
             <CardQuote task ={item.task}
               background={item.background}
+              bgColor={theme ? colors.cardDark : colors.cardLight}
+              txtColor={theme ? colors.txtDark : colors.txtLight}
             />
           }
         /> 
