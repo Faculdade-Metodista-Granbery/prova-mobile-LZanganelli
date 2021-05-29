9e74070b2e783654fa9ddffbb72e7291c673e6e0
Lucas Zanganelli <lucasfzanganelli@gmail.com>
2021-05-28 23:34:27 -0300

    card.component.jsx
    Adicionei a view no import do react native.
    Importei o progress bar
    e o import do arquivo colors.js
    Adicionei objetos na funçao Cardquote
    Adicionei o use state para o play
    Adicionei o progress para a barra

--- components/card/card.component.jsx
+++ components/card/card.component.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Card, Paragraph, Button } from 'react-native-paper';
+import { ProgressBar } from 'react-native-paper';
+import {colors} from '../../utils/colors';
+
 
 
 const styles = StyleSheet.create({
     card: {
-        backgroundColor: '#EBF5F7',
+        // backgroundColor: '#EBF5F7',
         margin: 10,
         borderRadius: 20,
         flex: 1,
@@ -13,40 +16,61 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
     },
     content: {
-        backgroundColor: '#FAE8E0',
+        width: '100%', 
+        alignItems: 'center',
     },
     cover: {
         height: 220,
     },
 });
 
-const CardQuote = ({task, background}) => {
-const [likeButton, setLikebutton] = useState("play");  
-const handleLike = () => {
-    if (likeButton === "play") {
-        setLikebutton("math-norm")
-    } else {
-        setLikebutton("play")
-    }
-}  
+const CardQuote = ({task, background, bgColor, txtColor}) => {
+    const [likeButton, setLikebutton] = useState("play");
+    const [progress, setProgress] = useState(0)
+    
+    const handleLike = () => {
+        
+        if (likeButton === "play") {
+            setLikebutton("math-norm")
+            setProgress(1)
+        } else {
+            setLikebutton("play")
+            setProgress(0)
+        }
+    }  
 
     return (
-        <Card style={styles.card}>
+        <Card style={[{backgroundColor: bgColor}, styles.card]}>
             <Card.Cover
                 style={styles.cover}
                 resizeMode={`cover`}
-                source={{ uri: background }} />
-                <Paragraph>{task}</Paragraph>
-            <Card.Content style={styles.content}>
+                source={{ uri: background }} 
+            />
+            <Card.Content 
+                style={styles.content}
+            >
+                <Paragraph 
+                    style={{color: txtColor}}
+                >
+                    {task}
+                </Paragraph>
+
+                <Card.Actions>
+                    <Button 
+                        onPress={handleLike}
+                        icon={likeButton}
+                        labelStyle={{fontSize: 50}}
+                    />                    
+                </Card.Actions>
             </Card.Content>
-            <Card.Actions>
-                <Button onPress={handleLike}
-                icon={likeButton}
-                labelStyle={{fontSize: 70}}
-                />
-                
-            </Card.Actions>
+            <ProgressBar
+                progress={progress}
+                color={colors.txtColor}
+                style={{ height: 10, width: '100%'}}
+                borderRadius={50}>
+            </ProgressBar>
         </Card>
+            
     )
 }
 
